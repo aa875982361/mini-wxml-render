89ad6e726b25ea24e58ae4e05bf877c9e6032aa4
huangpeilin <875982361@qq.com>
2022-06-09 00:18:13 +0800

    feat: 增加appjson 的类型

--- src/global.ts
+++ src/global.ts
@@ -4,3 +4,8 @@ export const contentKey = isProd ? "_b" : "content"
 export const tagNameKey = isProd ? "_c" :  "tagName"
 export const attributesKey = isProd ? "_d":  "attributes"
 export const vdomsKey = isProd ? "_e":  "vdoms"
+
+export interface AppJson {
+  pages: string[],
+  
+}
